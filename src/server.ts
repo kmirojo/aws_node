a09e337fa9d71bef2d180ce23d5457d0f59da5bd
juancamilo.rojas <juancamilo.rojas@CO-IT014552.globant.com>
2021-04-21 09:31:06 -0500

    Modified text

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World with express!"));
+app.get("/", (req, res) => res.send("<h1>Express with HTML</h1>"));
 
 app.listen(3000);
 console.log("Server on Port 3000");
